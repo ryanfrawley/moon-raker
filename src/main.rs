5aa158dfa10f3c77d3d01bc49e5fe66035490473
Ryan Frawley <me@ryanfrawley.io>
2023-07-30 11:37:00 -0400

    fix: file path

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod document;
 // mod xpath;
 
 fn main() {
-    let s = include_str!("test.txt");
+    let s = include_str!("books.xml");
     let doc = document::build_document_tree(s);
     document::print_node(0, &doc);
 }
